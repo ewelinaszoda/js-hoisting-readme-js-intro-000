82e85bf998d1b0d7bac0f8993129920400884418
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-23 21:32:01 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,8 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  var thisIsCrazy() = function (){
     console.log("hey!!!")
     thisIsCrazy();
   }
@@ -20,13 +19,12 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
   
 }
